e0cd7ebbc670759c3a17f6bd65a600fdfdf5acb5
daesang.cho <daesang.cho@samsung.com>
2017-03-06 18:54:05 +0900

    partial completion ch6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [ AppComponent,
-                  HeroDetailComponent
+                  HeroDetailComponent,
+                  HeroesComponent,
+                  DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap:    [ AppComponent ]
 })
